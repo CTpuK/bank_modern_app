9213390749186599022e69214bc0a0a58696c051
Evgeny Strikunov <strik.jenya@gmail.com>
2023-12-14 12:41:22 +0300

    feat: 14.12

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,12 @@
-function Button() {
-  return <div>Button</div>;
+function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`bg-blue-gradient px-6 py-4 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default Button;
